196ce76a6a6917a721d0ec96f525c03eff8f4450
yingshuai <yingshuai>
2018-01-24 17:37:58 +0800

    初始化项目配置

---
+++ cms-service/src/main/java/com/dreamtale/cms/service/CmsServiceApp.java
@@ -0,0 +1,12 @@
+package com.dreamtale.cms.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsServiceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmsServiceApp.class, args);
+	}
+}
